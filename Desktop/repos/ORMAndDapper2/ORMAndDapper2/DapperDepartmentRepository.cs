e7dd2c6088efc1325a64a8c3e93ec3d0d92ecd64
Andrew <andrewhubbard78@gmail.com>
2023-10-12 11:20:12 -0400

    A

---
+++ Desktop/repos/ORMAndDapper2/ORMAndDapper2/DapperDepartmentRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace ORMAndDapper2
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public DapperDepartmentRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+            public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<Department>("SELECT * FROM Departments;");
+        }
+
+
+    }
+}
