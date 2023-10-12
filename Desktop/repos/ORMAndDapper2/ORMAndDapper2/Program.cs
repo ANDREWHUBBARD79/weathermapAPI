0657d7d60ff9fdf4bb4db47f4d54357d576f68ab
Andrew <andrewhubbard78@gmail.com>
2023-10-12 11:52:07 -0400

    A

--- Desktop/repos/ORMAndDapper2/ORMAndDapper2/Program.cs
+++ Desktop/repos/ORMAndDapper2/ORMAndDapper2/Program.cs
@@ -5,9 +5,32 @@ using MySql.Data.MySqlClient;
 using Microsoft.Extensions.Configuration;
 //^^^^MUST HAVE USING DIRECTIVES^^^^
 
-var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
-string connString = config.GetConnectionString("DefaultConnection");
-IDbConnection conn = new MySqlConnection(connString);
+namespace ORMAndDapper2
+
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+
+        string connString = config.GetConnectionString("DefaultConnection");
+
+         IDbConnection conn = new MySqlConnection(connString);
+
+        var repo = new DapperDepartmentRepository(conn);
+
+        var departments = repo.GetAllDepartments();
+
+        foreach (var department in departments)
+        {
+            Console.WriteLine($"{dept.DepartmentID} {dept.Name}");
+        }
+
+        repo.GetAllDepartments();
+
+    }
+}
