e1c84e380526ae2208ddb9d109ccfe367b0c0205
Andrew <andrewhubbard78@gmail.com>
2023-11-12 14:29:17 -0500

    A

---
+++ Desktop/repos/AndrewsCRUD/AndrewsCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AndrewsCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
